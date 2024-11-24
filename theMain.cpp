8fb0ddb4a5ceece6e12c564fe3856270aa40d4e8
Youetsu Yoshida <y.sato@jc-21.jp>
2024-11-25 03:32:38 +0900

    表示まで

--- theMain.cpp
+++ theMain.cpp
@@ -2,12 +2,18 @@
 #include "Input.h"
 #include "deque"
 
+class snake;
+
 namespace
 {
 	const int WIN_WIDTH = 1024;
 	const int WIN_HEIGHT = 768;
 	int crrTime;
 	int prevTime;
+
+	int BOXSIZE{ 20 };
+	snake *s;
+
 }
 
 enum DIR
@@ -15,11 +21,15 @@ enum DIR
 	UP, LEFT, DOWN, RIGHT, NONE, MAXDIR
 };
 
+
 struct pos
 {
 	int x;
 	int y;
 };
+
+pos dirs[] = { {0,-1},{-1, 0}, {0, 1}, {1, 0}, {0, 0} };
+
 
 class sbody
 {
@@ -30,10 +40,11 @@ public:
 	sbody(int x, int y);
 	void SetPosition(int x, int y);
 	pos GetPosition();
-	void SetForward(DIR dir) { fwrdDir = dir; };
-	DIR GetFoward() { return fwrdDir; };
-	DIR GetBakward() { return (DIR)((fwrdDir + 2) % 4); };
+	void SetForward(DIR dir) { fwrdDir = dir; }
+	DIR GetFoward() { return fwrdDir; }
+	DIR GetBakward() { return (DIR)((fwrdDir + 2) % 4); }
 };
+
 sbody::sbody()
 {
 	spos = { 0, 0 };
@@ -60,23 +71,40 @@ pos sbody::GetPosition()
 class snake
 {
 	std::deque<sbody> body;
+	enum 
+	{
+		INIT, PLAY, MOVE, STOP, DEATH, MAXSSTATE 
+	}sstate;
 public:
 	snake();
 	void AddBody(DIR dir);
 	void Move();
 	void Update();
 	void Draw();
-	DIR GetFwrdDir() { body.front(); };
+	DIR GetFwrdDir() { body.front().GetFoward(); };
 };
 
 snake::snake()
 {
+	for (int i = 0; i < 3; i++)
+	{
+		sbody bd;
+		bd.SetPosition(10-i, 10);
+		bd.SetForward(RIGHT);
+		body.push_back(bd);
+	}
+	sstate = INIT;
 }
 
+//自分が動いたあとに１個増える
 void snake::AddBody(DIR dir)
 {
-	DIR topDir = body.front().GetFoward();
-	body.push_front({});
+	DIR addDir = body.back().GetBakward();
+	DIR tailDir = body.back().GetFoward();
+	pos tailPos = body.back().GetPosition();
+	pos addPos = { tailPos.x + dirs[dir].x, tailPos.y + dirs[dir].y };
+	sbody newBody(addPos.x, addPos.y);
+	body.push_back(newBody);
 }
 
 void snake::Move()
@@ -85,10 +113,23 @@ void snake::Move()
 
 void snake::Update()
 {
+	if (sstate == INIT)
+	{
+		if (Input::IsKeyDown(KEY_INPUT_RIGHT))
+		{
+
+		}
+	}
 }
 
 void snake::Draw()
 {
+	for (auto& itr : body)
+	{
+		pos p = itr.GetPosition();
+		DrawBox(p.x * BOXSIZE, p.y * BOXSIZE, (p.x + 1) * BOXSIZE, (p.y + 1) * BOXSIZE, GetColor(0, 255, 0), TRUE);
+		DrawBox(p.x * BOXSIZE, p.y * BOXSIZE, (p.x + 1) * BOXSIZE, (p.y + 1) * BOXSIZE, GetColor(0, 0, 255), FALSE);
+	}
 }
 
 
@@ -134,16 +175,18 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	crrTime = GetNowCount();
 	prevTime = GetNowCount();
 
-
+	s = new snake;
 	while (true)
 	{
 		ClearDrawScreen();
 		Input::KeyStateUpdate();
 		//ここにやりたい処理を書く
 		crrTime = GetNowCount();
-		float deltaTime = (crrTime - prevTime) / 1000.0;//フレーム間時間（ｓ）
+		float deltaTime = (float)(crrTime - prevTime) / 1000.0;//フレーム間時間（ｓ）
 		
-
+		Init();
+		Update(deltaTime);
+		Draw(deltaTime);
 
 
 
@@ -172,5 +215,13 @@ void Update(float dTime)
 
 void Draw(float dTime)
 {
+	//Stage
+	for (int j = 0; j < WIN_HEIGHT; j += 20) {
+		for (int i = 0; i < WIN_WIDTH; i += 20) {
+			DrawBox(i, j, i + 20, j + 20, GetColor(255, 0, 0), FALSE);
+		}
+
+	}
+	s->Draw();
 }
 
